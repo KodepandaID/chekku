6c046c97667582cd4dfb30cd8032ac6f54632af3
LordAur <yudha.webdev@gmail.com>
2020-09-10 11:41:21 +0700

    Added new test rules isIPv4

---
+++ test/rules/is_ipv4_test.go
@@ -0,0 +1,146 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/KodepandaID/chekku"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsIPv4InvalidText(t *testing.T) {
+	type IP struct {
+		ip string `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: "hello",
+	})
+
+	if e == nil {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be ip address version 4`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
+
+func TestIsIPv4InvalidWithInteger(t *testing.T) {
+	type IP struct {
+		ip int `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: 123,
+	})
+
+	if e == nil {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be string`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
+
+func TestIsIPv4InvalidIP(t *testing.T) {
+	type IP struct {
+		ip string `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: "1.2.3.a",
+	})
+
+	if e == nil {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be ip address version 4`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
+
+func TestIsIPv4InvalidIPRules(t *testing.T) {
+	type IP struct {
+		ip string `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: "1.2.3.256",
+	})
+
+	if e == nil {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be ip address version 4`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
+
+func TestIsIPv4InvalidWithIPv6(t *testing.T) {
+	type IP struct {
+		ip string `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210",
+	})
+
+	if e == nil {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be ip address version 4`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
+
+func TestIsIPv4Valid(t *testing.T) {
+	type IP struct {
+		ip string `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: "0.1.2.3",
+	})
+
+	if e == nil {
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be ip address version 4`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
+
+func TestIsIPv4ValidSecond(t *testing.T) {
+	type IP struct {
+		ip string `chekku:"isIPv4"`
+	}
+
+	e := chekku.Validate(IP{
+		ip: "255.255.255.255",
+	})
+
+	if e == nil {
+		return
+	}
+
+	if !assert.Equal(t, e.Error(), `"isIPv4", ip value must be ip address version 4`) {
+		t.Error("isIPv4 shoud be invalid")
+		return
+	}
+}
