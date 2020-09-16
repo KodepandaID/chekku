a6fc174d9554fb579fbb351eb23d8c68877311a6
LordAur <yudha.webdev@gmail.com>
2020-09-16 10:18:55 +0700

    Added rules endWith

---
+++ test/rules/end_with_test.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/KodepandaID/chekku"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEndWithInvalid(t *testing.T) {
+	type Str struct {
+		str string `chekku:"endWith:foo"`
+	}
+
+	invalidValues := []string{
+		"",
+		"foobar",
+	}
+
+	for _, v := range *&invalidValues {
+		e := chekku.Validate(Str{
+			str: v,
+		})
+
+		if e == nil {
+			t.Error("endWith shoud be invalid")
+			return
+		}
+
+		if !assert.Equal(t, e.Error(), `"endWith", str value must be end with foo`) {
+			t.Error("endWith shoud be invalid")
+			return
+		}
+	}
+}
+
+func TestEndWithValid(t *testing.T) {
+	type Str struct {
+		str string `chekku:"endWith:foo"`
+	}
+
+	validValues := []string{
+		"bar foo",
+		"fighter foo",
+	}
+
+	for _, v := range *&validValues {
+		e := chekku.Validate(Str{
+			str: v,
+		})
+
+		if e != nil {
+			t.Error("endWith shoud be valid")
+			return
+		}
+	}
+}
