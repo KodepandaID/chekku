e6f971f740e969ab6903cf1ed0e6538df363e689
Yudha Pratama <yudha.webdev@gmail.com>
2021-07-17 21:58:08 +0700

    Fixed, error requiredIf and requiredUnless

--- pkg/rules/required.go
+++ pkg/rules/required.go
@@ -10,40 +10,38 @@ import (
 // RequiredIf to check another field is a match value
 func (r Rules) RequiredIf(fieldName string, v reflect.Value, m string) error {
 	condition := strings.Split(m, ",")
-	z := r.Inputs.FieldByName(condition[0])
-
 	e := r.Required(fieldName, v)
 
-	switch z.Kind() {
+	switch v.Kind() {
 	case reflect.Bool:
 		b, _ := strconv.ParseBool(condition[1])
-		if z.Bool() == b && e != nil {
+		if v.Bool() == b && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	case reflect.Float32, reflect.Float64:
 		f, _ := strconv.ParseFloat(condition[1], 64)
-		if z.Float() == f && e != nil {
+		if v.Float() == f && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	case reflect.String:
-		if z.String() == string(condition[1]) && e != nil {
+		if v.String() == string(condition[1]) && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		i, _ := strconv.ParseInt(condition[1], 10, 64)
-		if z.Int() == i && e != nil {
+		if v.Int() == i && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	default:
-		if z.IsNil() {
+		if v.IsNil() {
 			return fmt.Errorf("\"required\", %v is required", fieldName)
 		}
 
@@ -54,40 +52,38 @@ func (r Rules) RequiredIf(fieldName string, v reflect.Value, m string) error {
 // RequiredUnless to check another field is a doesn't match value
 func (r Rules) RequiredUnless(fieldName string, v reflect.Value, m string) error {
 	condition := strings.Split(m, ",")
-	z := r.Inputs.FieldByName(condition[0])
-
 	e := r.Required(fieldName, v)
 
-	switch z.Kind() {
+	switch v.Kind() {
 	case reflect.Bool:
 		b, _ := strconv.ParseBool(condition[1])
-		if z.Bool() != b && e != nil {
+		if v.Bool() != b && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	case reflect.Float32, reflect.Float64:
 		f, _ := strconv.ParseFloat(condition[1], 64)
-		if z.Float() != f && e != nil {
+		if v.Float() != f && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	case reflect.String:
-		if z.String() != string(condition[1]) && e != nil {
+		if v.String() != string(condition[1]) && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		i, _ := strconv.ParseInt(condition[1], 10, 64)
-		if z.Int() != i && e != nil {
+		if v.Int() != i && e != nil {
 			return fmt.Errorf("%v", e)
 		}
 
 		return nil
 	default:
-		if z.IsNil() {
+		if v.IsNil() {
 			return fmt.Errorf("\"required\", %v is required", fieldName)
 		}
 
